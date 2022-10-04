7d9024763da36eeee95c26b21fe13538f5ba63b8
akashkuchankar <kuchankarakash@gmail.com>
2022-10-04 07:40:09 -0700

    Add project files.

---
+++ reverse number 100 to 50 by do while loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_number_100_to_50_by_do_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i= 100;
+            do
+            {
+                Console.WriteLine(i);
+                i--;
+
+            } while (i>=50);
+
+
+        }
+    }
+}
